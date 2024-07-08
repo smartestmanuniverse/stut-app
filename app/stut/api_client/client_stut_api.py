1a4d30532392b8321eff5fcd4ac2f0623276cd1d
0x07cb <83157348+0x07CB@users.noreply.github.com>
2024-07-08 17:12:11 +0200

    updates around stut-app ui

--- app/stut/api_client/client_stut_api.py
+++ app/stut/api_client/client_stut_api.py
@@ -1,11 +1,2 @@
 #coding: utf-8
 
-import requests
-
-r = requests.post('http://127.0.0.1:5000/login', 
-                  json={"username":"rick", "password":"1234"})
-if r.status_code == 200:
-    r = requests.get('http://127.0.0.1:5000/login/status',
-                     headers={"Authorization": "Bearer " + r.json()['access_token']})
-    
-    print(r.json())
